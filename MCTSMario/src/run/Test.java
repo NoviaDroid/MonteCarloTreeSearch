1e34f64952310c54254e5e7187f5f31b26535b27
huiwq1990 <huiwq1990@163.com>
2013-12-27 00:01:43 +0800

    s

---
+++ MCTSMario/src/run/Test.java
@@ -0,0 +1,14 @@
+package run;
+
+import itu.ejuuragr.MiniStats2;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		new MiniStats2();
+		
+		 new HardStats("tu.ejuuragr.RandomMCTSAgent",); i 0 6 0.25 0.125 1 0 1 1 1 1
+	}
+
+}
